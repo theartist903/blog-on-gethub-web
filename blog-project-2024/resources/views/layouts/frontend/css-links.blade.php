9dbd32f1e0f29190580d65da7984534733962353
212194ubaid <Ubaidullah4453@gmail.com>
2024-09-27 17:02:31 +0500

    index page frontend layout completed

---
+++ blog-project-2024/resources/views/layouts/frontend/css-links.blade.php
@@ -0,0 +1,45 @@
+  <!-- Favicons -->
+  <link href="{{asset('frontend/assets/img/favicon.png')}}" rel="icon">
+
+  <link href="{{asset('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+ 
+<link href="{{asset('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <link href="{{asset('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Inter:wght@400;500&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS Files -->
+  <link href="{{asset('frontend/assets/css/variables.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/css/main.css')}}" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="{{asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+<link href="{{asset('frontend/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+<link href="{{asset('frontend/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+
+<!-- Template Main CSS Files -->
+<link href="{{asset('frontend/assets/css/variables.css')}}" rel="stylesheet">
+<link href="{{asset('frontend/assets/css/main.css')}}" rel="stylesheet">
+
+
+
+  <!-- =======================================================
+  * Template Name: ZenBlog
+  * Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https:///bootstrapmade.com/license/
+  ======================================================== -->
