6d85d4729536bfc6ccfeaaa36d6e50cf1a5cbd20
Abdull2233 <shahabdull426@gmail.com>
2024-09-30 14:53:00 +0500

    migrations updated
    columns datatypes are updated and new column is added

--- blog-project-2024/database/migrations/2024_09_26_111255_create_categories_table.php
+++ blog-project-2024/database/migrations/2024_09_26_111255_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
+            $table->boolean('status')->default(1);
             $table->timestamps();
         });
     }
