10fae4c4ccc1a41d3e80af373bc9c34ac9c49300
212194ubaid <Ubaidullah4453@gmail.com>
2024-10-01 15:34:49 +0500

    ubaid frontend layout pages completed

--- blog-project-2024/routes/web.php
+++ blog-project-2024/routes/web.php
@@ -123,34 +123,5 @@ Route::get('/components-carousel', function () {
 
 
 
-Route::get('frontend-about', function () {
-    return view('frontend/about');
-})->name('frontend-about');
 
-
-
-
-Route::get('frontend-category', function () {
-    return view('frontend/category');
-});
-
-Route::get('frontend-contact', function () {
-    return view('frontend/contact');
-})->name('frontend-contact');
-
-Route::get('frontend-search', function () {
-    return view('frontend/search');
-})->name('frontend-search');
-
-
-Route::get('frontend-single-post', function () {
-    return view('frontend/single-post');
-})->name('frontend-single-post');
-
-
-
-Route::get('frontend-index', function () {
-    return view('frontend/index');
-});
-
-
+require __DIR__ . '/ubaid.php';
